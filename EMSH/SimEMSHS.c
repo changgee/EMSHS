ff6cfa383bf69b7679ec5f929c804060993f7af9
Changgee Chang <changgee@mercury.pmacs.upenn.edu>
2017-09-11 13:43:20 -0400

    abc

--- EMSH/SimEMSHS.c
+++ EMSH/SimEMSHS.c
@@ -106,8 +106,8 @@ int main()
 
 			if ( p == 1000 )
 			{
-				fputs("mu = 10:16/3+0.1\n",f);
-				fputs("nu = c(1:5*0.01,0:7/4+0.1)\n",f);
+				fputs("mu = 10:15/3+0.1\n",f);
+				fputs("nu = 1:5/2-0.15\n",f);
 			}
 			else
 			{
